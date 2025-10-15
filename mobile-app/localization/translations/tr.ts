ae493db15b2e2d5a33ab69c284fe5a4a69eaabb6
Halil <99981913+Halil777@users.noreply.github.com>
2025-10-15 10:08:22 +0300

    Add new localization options for mobile app

---
+++ mobile-app/localization/translations/tr.ts
@@ -0,0 +1,17 @@
+import { TranslationDictionary } from './types';
+
+const tr: TranslationDictionary = {
+  common: {
+    language: {
+      english: 'İngilizce',
+      russian: 'Rusça',
+      turkish: 'Türkçe',
+      turkmen: 'Türkmence',
+      chinese: 'Çince',
+      switcherLabel: 'Dil: {{language}}',
+      switcherAccessibility: 'Uygulama dilini değiştir',
+    },
+  },
+};
+
+export default tr;
